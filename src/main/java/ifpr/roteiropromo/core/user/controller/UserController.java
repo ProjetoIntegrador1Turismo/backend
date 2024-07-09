fe1c60d10e658dc7e2c5d6c16e05f3d06577e9e7
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-09 15:49:56 -0300

    chora: comment untested methods and set preAuthorize role

--- src/main/java/ifpr/roteiropromo/core/user/controller/UserController.java
+++ src/main/java/ifpr/roteiropromo/core/user/controller/UserController.java
@@ -8,6 +8,7 @@ import ifpr.roteiropromo.core.user.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,45 +24,48 @@ public class UserController {
         this.userService = userService;
     }
 
-    //Create a new user
+    //Cria um novo usuário - Turista ou Guia
     @PostMapping("/create")
     public ResponseEntity<UserDTO> createNewUser(@RequestBody UserDTOForm userDTOForm){
         return ResponseEntity.ok(userService.creatNewUser(userDTOForm));
     }
 
-    //Metodo pausado. Ponderar a rota já pronta para disparar o email de recuperação!
-    @GetMapping("/recovery")
-    public ResponseEntity<String> resetPassword(
-            @RequestBody UserDTORecovery userDTORecovery
-            ){
-        return ResponseEntity.ok(userService.resetUserPassword(userDTORecovery));
-    }
-
-
-    //Metodo só para testes. Provavelmente não será mantido.
-//    @GetMapping()
-//    public String getAdminToken(){
-//        JwtTokenHandler jwtTokenHandler = new JwtTokenHandler();
-//        return jwtTokenHandler.getAdminToken();
-//    }
-
-
+    //Recupera todos os usuários - Guia ou Turista. Acesso ADMIN apenas.
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.ok(userService.getAll());
     }
 
 
+    //Recupera um usuário pelo email - Acesso ADMIN apenas.
     @GetMapping("/email")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<User> getOne(@RequestParam String email){
         return ResponseEntity.ok(userService.getOneByEmail(email));
     }
 
     @PutMapping
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<User> updateUser(@RequestBody UserDTO userDTO){
         return ResponseEntity.ok(userService.update(userDTO));
     }
 
+    //    //Metodo pausado. Ponderar a rota já pronta para disparar o email de recuperação!
+//    @GetMapping("/recovery")
+//    public ResponseEntity<String> resetPassword(
+//            @RequestBody UserDTORecovery userDTORecovery
+//            ){
+//        return ResponseEntity.ok(userService.resetUserPassword(userDTORecovery));
+//    }
+
+
+    //Metodo só para testes.
+//    @GetMapping()
+//    public String getAdminToken(){
+//        JwtTokenHandler jwtTokenHandler = new JwtTokenHandler();
+//        return jwtTokenHandler.getAdminToken();
+//    }
 
 
 
