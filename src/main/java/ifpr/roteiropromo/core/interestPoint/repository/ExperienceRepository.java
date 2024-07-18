db2a417e5a51f7c45761505844b7af46665035bf
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-18 20:19:29 -0300

    feat: create tourist point and experience repository

---
+++ src/main/java/ifpr/roteiropromo/core/interestPoint/repository/ExperienceRepository.java
@@ -0,0 +1,7 @@
+package ifpr.roteiropromo.core.interestPoint.repository;
+
+import ifpr.roteiropromo.core.interestPoint.domain.entities.Experience;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperienceRepository extends JpaRepository<Experience, Long>{
+}
