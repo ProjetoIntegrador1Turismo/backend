d40f03e370584ef93096cd2ac3e4b52e86656c3b
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-18 20:16:44 -0300

    chore: fill first slider with touristPoints, experiences and itinerary's

--- src/main/java/ifpr/roteiropromo/core/pagesource/service/PageSourceService.java
+++ src/main/java/ifpr/roteiropromo/core/pagesource/service/PageSourceService.java
@@ -1,39 +1,94 @@
 package ifpr.roteiropromo.core.pagesource.service;
 
+import ifpr.roteiropromo.core.interestPoint.domain.entities.Experience;
 import ifpr.roteiropromo.core.interestPoint.domain.entities.InterestPoint;
+import ifpr.roteiropromo.core.interestPoint.domain.entities.TouristPoint;
+import ifpr.roteiropromo.core.interestPoint.repository.ExperienceRepository;
+import ifpr.roteiropromo.core.interestPoint.repository.TouristPointRepository;
 import ifpr.roteiropromo.core.interestPoint.service.InterestPointService;
+import ifpr.roteiropromo.core.itinerary.domain.entities.Itinerary;
+import ifpr.roteiropromo.core.itinerary.repository.ItineraryRepository;
+import ifpr.roteiropromo.core.pagesource.domain.BasicPointDTO;
 import ifpr.roteiropromo.core.pagesource.domain.HomePageDTO;
 import ifpr.roteiropromo.core.pagesource.domain.InterestPointCardDTO;
+import ifpr.roteiropromo.core.pagesource.domain.SliderDTO;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PageSourceService {
 
     private final InterestPointService interestPointService;
+    private final TouristPointRepository touristPointRepository;
+    private final ExperienceRepository experienceRepository;
+    private final ItineraryRepository itineraryRepository;
     private final ModelMapper modelMapper;
 
     public HomePageDTO getHomePageData(){
         HomePageDTO homePageDTO = new HomePageDTO();
         homePageDTO.setTop3InterestPoints(getTop3InterestPoints());
+        homePageDTO.setFirstSlider(getRandomPointsToFirstSlider()); ////roteiros ou experiencias
+
         return homePageDTO;
     }
+
+    private SliderDTO getRandomPointsToFirstSlider() {
+        SliderDTO sliderDTO = new SliderDTO();
+        List<TouristPoint> touristPoints = touristPointRepository.findAll();
+        Collections.shuffle(touristPoints);
+        List<TouristPoint> random3TouristPoints = touristPoints.subList(0, 3);
+        sliderDTO.setBasicPointDTOList(mapList(random3TouristPoints, BasicPointDTO.class));
+
+        List<Experience> experiences = experienceRepository.findAll();
+        Collections.shuffle(touristPoints);
+        List<Experience> random3Experiences = experiences.subList(0, 3);
+        sliderDTO.getBasicPointDTOList().addAll(mapList(random3Experiences, BasicPointDTO.class));
+
+        sliderDTO.getBasicPointDTOList().addAll(getRandomItineraries());
+
+
+        return sliderDTO;
+    }
+
+    private List<BasicPointDTO> getRandomItineraries() {
+        List<Itinerary> itineraries = itineraryRepository.findAll();
+        Collections.shuffle(itineraries);
+        List<Itinerary> random3Itineraries = itineraries.subList(0, 3);
+        List<BasicPointDTO> basicPointDTO = new ArrayList<>();
+        for (Itinerary itinerary : random3Itineraries) {
+            BasicPointDTO basicPoint = new BasicPointDTO();
+            basicPoint.setId(itinerary.getId());
+            basicPoint.setName(itinerary.getTitle());
+            basicPointDTO.add(basicPoint);
+        }
+        return basicPointDTO;
+    }
+
 
     //Ajustar para capturar o ponto de interesse no arquivo de configuração
     private List<InterestPointCardDTO> getTop3InterestPoints() {
         List<InterestPointCardDTO> top3InterestPoints = new ArrayList<>();
-//        InterestPoint cataratas = interestPointService.getOne(252L);
-//        InterestPoint itapu = interestPointService.getOne(302L);
+        InterestPoint cataratas = interestPointService.getOne(3L);
+        InterestPoint itapu = interestPointService.getOne(4L);
         InterestPoint parque = interestPointService.getOne(2L);
-//        top3InterestPoints.add(modelMapper.map(cataratas, InterestPointCardDTO.class));
-//        top3InterestPoints.add(modelMapper.map(itapu, InterestPointCardDTO.class));
+        top3InterestPoints.add(modelMapper.map(cataratas, InterestPointCardDTO.class));
+        top3InterestPoints.add(modelMapper.map(itapu, InterestPointCardDTO.class));
         top3InterestPoints.add(modelMapper.map(parque, InterestPointCardDTO.class));
         return top3InterestPoints;
+    }
+
+    public <S, D> List<D> mapList(List<S> source, Class<D> destinationType) {
+        return source.stream()
+                .map(element -> modelMapper.map(element, destinationType))
+                .collect(Collectors.toList());
     }
 
 
