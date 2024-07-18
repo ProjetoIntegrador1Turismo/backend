e9bf1de66658434ab5b5e1296eab0b5421b07afe
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-18 20:17:23 -0300

    feat: create a basic DTO to fill sliders on home page

---
+++ src/main/java/ifpr/roteiropromo/core/pagesource/domain/BasicPointDTO.java
@@ -0,0 +1,14 @@
+package ifpr.roteiropromo.core.pagesource.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BasicPointDTO {
+
+    private Long id;
+    private String name;
+    private String imageCoverUrl;
+}
