59f38315f07bb2c27fd1b6fc7df74f9cfb9de9cf
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-07-30 12:20:34 -0300

    chore: create new routes to guide on GuideController

--- src/main/java/ifpr/roteiropromo/core/user/controller/GuideController.java
+++ src/main/java/ifpr/roteiropromo/core/user/controller/GuideController.java
@@ -1,26 +1,37 @@
 package ifpr.roteiropromo.core.user.controller;
+import ifpr.roteiropromo.core.user.domain.entities.Guide;
+import ifpr.roteiropromo.core.user.service.GuideService;
 import ifpr.roteiropromo.core.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
-@RequestMapping("/guide")
+@RequestMapping("/guides")
 @Log4j2
 @RequiredArgsConstructor
 public class GuideController {
 
-    private final UserService userService;
+    private final GuideService guideService;
 
-//    @PostMapping("/create")
-//    public ResponseEntity<Guide> createNewGuide(@RequestBody UserDTOForm userDTOForm){
-//        return ResponseEntity.ok(userService.createGuide(userDTOForm));
-//    }
-//
-//    @GetMapping()
-//    public ResponseEntity<List<Guide>> getOne(){
-//        return ResponseEntity.ok(userService.getAllGuides());
-//    }
+    @GetMapping()
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<List<Guide>> getAllGuides(){
+        return ResponseEntity.ok(guideService.getAllGuides());
+    }
+
+
+    @GetMapping("/test")
+    public List<Long> test(){
+        return guideService.getTopFiveGuidesId();
+    }
+
+
+
 
 }
