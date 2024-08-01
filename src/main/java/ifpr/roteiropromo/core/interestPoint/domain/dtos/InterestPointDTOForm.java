1b703583ef152ff6e75df99a16e0856942a01564
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-01 19:23:20 -0300

    chore: insert all attributes on interest point dto form

--- src/main/java/ifpr/roteiropromo/core/interestPoint/domain/dtos/InterestPointDTOForm.java
+++ src/main/java/ifpr/roteiropromo/core/interestPoint/domain/dtos/InterestPointDTOForm.java
@@ -1,5 +1,7 @@
 package ifpr.roteiropromo.core.interestPoint.domain.dtos;
 
+import ifpr.roteiropromo.core.address.model.dtos.AddressDTO;
+import ifpr.roteiropromo.core.address.model.entities.Address;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +14,16 @@ public class InterestPointDTOForm {
     String shortDescription;
     String interestPointType;
     String imageCoverUrl;
+    String duration;
+    String averageValue;
+    AddressDTO address;
+    String longDescription;
+    String requiredAge;
+    String date;
+    Integer starsNumber;
+    Boolean isResort;
+    Boolean breakfastIncluded;
+    String foodType;
 }
+
+
