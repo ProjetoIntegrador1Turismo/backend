2c67362d33e0b305010ba340e068fb6a0ce807f0
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-05 17:09:37 -0300

    chore: add filter to log requests and show token

--- src/main/java/ifpr/roteiropromo/core/security/SecurityConfig.java
+++ src/main/java/ifpr/roteiropromo/core/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ifpr.roteiropromo.core.security;
 
+
+import ifpr.roteiropromo.core.utils.LoggingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
