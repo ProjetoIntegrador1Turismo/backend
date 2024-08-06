25ddc6375608471dd33510e053ed3dd4ee4d2bc2
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-06 17:33:14 -0300

    chore: create test route to create InterestPoint with data and images

--- src/main/java/ifpr/roteiropromo/core/interestPoint/controller/InterestPointController.java
+++ src/main/java/ifpr/roteiropromo/core/interestPoint/controller/InterestPointController.java
@@ -1,14 +1,17 @@
 package ifpr.roteiropromo.core.interestPoint.controller;
 
-import ifpr.roteiropromo.core.interestPoint.domain.dtos.InterestPointDTO;
+import ifpr.roteiropromo.core.images.service.ImageService;
 import ifpr.roteiropromo.core.interestPoint.domain.dtos.InterestPointDTOForm;
+import ifpr.roteiropromo.core.interestPoint.domain.dtos.InterestPointUpdateDTO;
 import ifpr.roteiropromo.core.interestPoint.domain.entities.InterestPoint;
 import ifpr.roteiropromo.core.interestPoint.service.InterestPointService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,22 +20,34 @@ import java.util.List;
 public class InterestPointController {
 
     private final InterestPointService interestPointService;
+    private final ImageService imageService;
 
-    public InterestPointController(InterestPointService interestPointService){
+
+    public InterestPointController(InterestPointService interestPointService, ImageService imageService, ImageService imageService1){
         this.interestPointService = interestPointService;
+        this.imageService = imageService1;
     }
 
-    //Cria um novo pont de interesse com os dados básicos - NOME e TIPO
+    //Cria um novo ponto de interesse com todos os dados
     @PostMapping()
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<InterestPoint> createNewInterestPoint(
-            @RequestBody InterestPointDTOForm interestPointDTOForm
-    ){
+    public ResponseEntity<InterestPoint> createNewInterestPoint(@RequestBody InterestPointDTOForm interestPointDTOForm){
         return ResponseEntity.ok(interestPointService.create(interestPointDTOForm));
     }
 
+    @PostMapping("/test")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void createNewInterestPointTeste(@RequestParam("dados") String dadosJson, @RequestParam MultipartFile file) throws IOException {
+        //return ResponseEntity.ok(interestPointService.create(dados));
+        String imageUrl = imageService.saveImage(file);
+        log.info( "LINK DA IMAGEM SALVA" + imageUrl);
+        log.info(dadosJson);
+    }
+
+
+
     @GetMapping()
-    @PreAuthorize("hasRole('USER')")
+    //@PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<InterestPoint>> getAllInterestPoints(){
         return ResponseEntity.ok(interestPointService.getAll());
     }
@@ -53,7 +68,7 @@ public class InterestPointController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<InterestPoint> updateOneById(
             @PathVariable Long id,
-            @RequestBody InterestPointDTO interestPointDTO
+            @RequestBody InterestPointUpdateDTO interestPointDTO
     ){
         return ResponseEntity.ok(interestPointService.update(id, interestPointDTO));
     }
