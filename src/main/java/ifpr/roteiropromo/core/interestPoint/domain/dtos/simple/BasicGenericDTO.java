df65096cedbd6581924bf2c7beb7f2c44aedf336
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-13 11:34:14 -0300

    refac: remove constructor and set lombok annotations

--- src/main/java/ifpr/roteiropromo/core/interestPoint/domain/dtos/simple/BasicGenericDTO.java
+++ src/main/java/ifpr/roteiropromo/core/interestPoint/domain/dtos/simple/BasicGenericDTO.java
@@ -1,21 +1,22 @@
 package ifpr.roteiropromo.core.interestPoint.domain.dtos.simple;
 
+import ifpr.roteiropromo.core.enums.InterestPointType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BasicGenericDTO {
 
     private Long id;
     private String name;
     private String shortDescription;
     private String imageCoverUrl;
+    private InterestPointType interestPointType;
 
-    public BasicGenericDTO(Long id, String imageCoverUrl, String name, String shortDescription) {
-        this.id = id;
-        this.imageCoverUrl = imageCoverUrl;
-        this.name = name;
-        this.shortDescription = shortDescription;
-    }
+
 }
