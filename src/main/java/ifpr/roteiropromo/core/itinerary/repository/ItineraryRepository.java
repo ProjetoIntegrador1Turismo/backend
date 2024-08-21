353c67a05c4b85954e93515198798ce43aa8dc25
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-21 15:16:57 -0300

    chore: create method to get itineraries paginated

--- src/main/java/ifpr/roteiropromo/core/itinerary/repository/ItineraryRepository.java
+++ src/main/java/ifpr/roteiropromo/core/itinerary/repository/ItineraryRepository.java
@@ -1,6 +1,12 @@
 package ifpr.roteiropromo.core.itinerary.repository;
 
 import ifpr.roteiropromo.core.itinerary.domain.entities.Itinerary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {}
+public interface ItineraryRepository extends JpaRepository<Itinerary, Long> {
+
+
+    Page<Itinerary> findItinerariesByTitleContainingIgnoreCase(String query, Pageable pageable);
+}
