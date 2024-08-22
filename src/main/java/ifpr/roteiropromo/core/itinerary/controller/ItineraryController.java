6471419e1cd6268f07315cf3bfd02c84469b8586
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-21 22:00:27 -0300

    chore: remove unnecessary method security

--- src/main/java/ifpr/roteiropromo/core/itinerary/controller/ItineraryController.java
+++ src/main/java/ifpr/roteiropromo/core/itinerary/controller/ItineraryController.java
@@ -39,7 +39,6 @@ public class ItineraryController {
     }
 
     @GetMapping()
-    //@PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<ItineraryDTO>> getAll(){
         return ResponseEntity.ok(itineraryService.findAll());
     }
