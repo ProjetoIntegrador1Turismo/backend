ac5f486b1f069d78b97c9f34aef95e7f0ff178ac
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-23 16:05:40 -0300

    chore: documentation of authentication and user routes

--- src/main/java/ifpr/roteiropromo/core/auth/controller/AuthenticationController.java
+++ src/main/java/ifpr/roteiropromo/core/auth/controller/AuthenticationController.java
@@ -3,6 +3,13 @@ package ifpr.roteiropromo.core.auth.controller;
 import ifpr.roteiropromo.core.auth.domain.AuthenticatedUserDTO;
 import ifpr.roteiropromo.core.auth.domain.UserAuthenticationDTO;
 import ifpr.roteiropromo.core.auth.service.AuthenticationService;
+import ifpr.roteiropromo.core.errors.StandartError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @Log4j2
+@Tag(name = "Authentication", description = "Operations related to user authentication")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -25,6 +32,16 @@ public class AuthenticationController {
 
     //Retorna os dados do usuario e o token de autenticação
     @PostMapping()
+    @Operation(summary = "Return user data and authentication token",
+            description = "Allow user to authenticate and receive a token to access the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "User not found or invalid authentication data",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "200", description = "User authenticated successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthenticatedUserDTO.class)) })
+    })
     public ResponseEntity<AuthenticatedUserDTO> getTokenAndUserData(@RequestBody UserAuthenticationDTO user){
         return ResponseEntity.ok(authenticationService.getUSerTokenAndData(user));
     }
--- src/main/java/ifpr/roteiropromo/core/user/controller/UserController.java
+++ src/main/java/ifpr/roteiropromo/core/user/controller/UserController.java
@@ -1,9 +1,16 @@
 package ifpr.roteiropromo.core.user.controller;
 
+import ifpr.roteiropromo.core.errors.StandartError;
 import ifpr.roteiropromo.core.user.domain.dtos.*;
-import ifpr.roteiropromo.core.user.domain.entities.Guide;
 import ifpr.roteiropromo.core.user.domain.entities.User;
 import ifpr.roteiropromo.core.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +22,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @Log4j2
+@Tag(name = "User", description = "Operations related to Users")
 public class UserController {
 
     private final UserService userService;
@@ -25,12 +33,38 @@ public class UserController {
 
 
     @PostMapping("/create")
+    @Operation(summary = "Create new User",
+            description = "Allow to create a new user of type: Tourist or Guide")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User created successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "409", description = "User email already registered",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid data for user creation",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
     public ResponseEntity<UserDTO> createNewUser(@RequestBody UserDTOForm userDTOForm){
         return ResponseEntity.ok(userService.createNewUser(userDTOForm));
     }
 
 
     @GetMapping
+    @Operation(summary = "Get all users",
+            description = "Allow ADMIN to get a list of all users registered.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of all users registered",
+                    content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = User.class)))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.ok(userService.getAll());
@@ -38,6 +72,22 @@ public class UserController {
 
 
     @GetMapping("/email")
+    @Operation(summary = "Find user by email",
+            description = "Allow ADMIN to get a users by email.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User data found by email",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Not found user by email provided",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<User> getOne(@RequestParam String email){
         return ResponseEntity.ok(userService.getOneByEmail(email));
@@ -45,8 +95,21 @@ public class UserController {
 
 
     @PutMapping("/update")
+    @Operation(summary = "Update user",
+            description = "Allow user to update data.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User data updated successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Not found user by email provided",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTOUpdate userDTOUpdate) {
-        log.debug("Received update request for user: {}", userDTOUpdate);
         UserDTO updatedUser = userService.updateUser(userDTOUpdate);
         return ResponseEntity.ok(updatedUser);
     }
@@ -55,8 +118,22 @@ public class UserController {
     // ENVIA e-mail de recuperação de senha para o usuário atual
     // OBS: PARA UTILIZAR ESSA ROTA É PRECISO JÁ ESTAR COM O CONTEINER NOVO ATUALIZADO... (O COM TEMA DO APP)
     @PostMapping("/recovery")
-    public ResponseEntity<String> resetPassword(@RequestBody UserDTORecovery userDTORecovery) {
-        return ResponseEntity.ok(userService.resetUserPassword(userDTORecovery));
+    @Operation(summary = "Recovery password",
+            description = "Allow user to recovery password by email.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Password recovery email sent successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "400", description = "Not found user by email provided",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<String> resetPassword(@RequestParam String email) {
+        return ResponseEntity.ok(userService.resetUserPassword(email));
     }
 
 
