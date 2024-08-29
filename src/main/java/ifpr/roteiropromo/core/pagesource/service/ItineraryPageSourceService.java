9c45e9e4335ed9dbdd182abe87d60f5dff23d661
Guilherme <guilherme_laz@hotmail.com>
2024-08-28 21:13:36 -0300

    chore: add ItineraryPageSource

---
+++ src/main/java/ifpr/roteiropromo/core/pagesource/service/ItineraryPageSourceService.java
@@ -0,0 +1,48 @@
+package ifpr.roteiropromo.core.pagesource.service;
+
+import ifpr.roteiropromo.core.itinerary.domain.dto.ItineraryDTO;
+import ifpr.roteiropromo.core.itinerary.service.ItineraryService;
+import ifpr.roteiropromo.core.pagesource.domain.BasicGuideDTO;
+import ifpr.roteiropromo.core.pagesource.domain.ItineraryPageDTO;
+import ifpr.roteiropromo.core.review.domain.DTO.ReviewDTO;
+import ifpr.roteiropromo.core.review.service.ReviewService;
+import ifpr.roteiropromo.core.user.domain.dtos.GuideDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItineraryPageSourceService {
+
+    private final ItineraryService itineraryService;
+    private final ReviewService reviewService;
+    private final ModelMapper modelMapper;
+
+    public ItineraryPageDTO getItineraryPageData(Long id){
+        ItineraryPageDTO itineraryPageDTO = new ItineraryPageDTO();
+        itineraryPageDTO.setItinerary(this.getItinerary(id));
+        itineraryPageDTO.setReviews(this.getReviews(id));
+        itineraryPageDTO.setGuide(this.getGuideByItineraryId(id));
+        return itineraryPageDTO;
+    }
+
+
+    private ItineraryDTO getItinerary(Long id){
+        return modelMapper.map(itineraryService.findOneById(id), ItineraryDTO.class);
+    }
+
+
+    private List<ReviewDTO> getReviews(Long id){
+        GuideDTO guideDTO = itineraryService.getGuideByItinerary(id).getGuide();
+        return reviewService.getAllByGuide(guideDTO.getId());
+    }
+
+
+    private BasicGuideDTO getGuideByItineraryId(Long id){
+        return modelMapper.map(itineraryService.getGuideByItinerary(id).getGuide(), BasicGuideDTO.class);
+    }
+
+}
