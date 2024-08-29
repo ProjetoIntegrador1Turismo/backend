62c4442a1c061c2844c36327127a2b34e30c7f8a
Guilherme <guilherme_laz@hotmail.com>
2024-08-29 13:33:57 -0300

    update: update ItineraryPageDTO to have List of GuideReviewDTO instead of ReviewDTO

--- src/main/java/ifpr/roteiropromo/core/pagesource/domain/ItineraryPageDTO.java
+++ src/main/java/ifpr/roteiropromo/core/pagesource/domain/ItineraryPageDTO.java
@@ -1,10 +1,8 @@
 package ifpr.roteiropromo.core.pagesource.domain;
 
 import ifpr.roteiropromo.core.itinerary.domain.dto.ItineraryDTO;
-import ifpr.roteiropromo.core.review.domain.DTO.ReviewDTO;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.List;
 
 @Getter
@@ -13,7 +11,6 @@ public class ItineraryPageDTO {
 
     private BasicGuideDTO guide;
     private ItineraryDTO itinerary;
-
-    private List<ReviewDTO> reviews;
+    private List<GuideReviewDTO> reviews;
 
 }
