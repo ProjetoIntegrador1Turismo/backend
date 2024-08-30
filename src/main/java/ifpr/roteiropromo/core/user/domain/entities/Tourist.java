1f01445f2aeb3d74f775ec6bd1301f5f4d4dd8a5
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-30 17:50:27 -0300

    feat: define the relation between Tourist and Itinerary

--- src/main/java/ifpr/roteiropromo/core/user/domain/entities/Tourist.java
+++ src/main/java/ifpr/roteiropromo/core/user/domain/entities/Tourist.java
@@ -1,10 +1,9 @@
 package ifpr.roteiropromo.core.user.domain.entities;
 
 import ifpr.roteiropromo.core.comments.domain.entities.Comment;
+import ifpr.roteiropromo.core.itinerary.domain.entities.Itinerary;
 import ifpr.roteiropromo.core.review.domain.entities.Review;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.List;
@@ -22,5 +21,13 @@ public class Tourist extends User {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews;
+
+    @ManyToMany(cascade = CascadeType.REMOVE)
+    @JoinTable(
+            name = "tourist_itinerary_interests",
+            joinColumns = @JoinColumn(name = "tourist_id"),
+            inverseJoinColumns = @JoinColumn(name = "itinerary_id")
+    )
+    private List<Itinerary> interestedItineraries;
 
     }
