4eb6ea07c6b66e50f05090ae45abf2bc5b596e2d
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-08-30 16:08:27 -0300

    chore: soft remove unnecessary routes to address

--- src/main/java/ifpr/roteiropromo/core/address/controller/AddressController.java
+++ src/main/java/ifpr/roteiropromo/core/address/controller/AddressController.java
@@ -1,28 +1,28 @@
-package ifpr.roteiropromo.core.address.controller;
-
-import ifpr.roteiropromo.core.address.model.entities.Address;
-import ifpr.roteiropromo.core.address.service.AddressService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/address")
-public class AddressController {
-
-
-    private final AddressService addressService;
-
-    public AddressController(AddressService addressService){
-        this.addressService = addressService;
-    }
-
-    public ResponseEntity<Address> creatOne(
-            @RequestBody Address address
-    ){
-        return ResponseEntity.ok(addressService.create(address));
-    }
-
-
-}
+//package ifpr.roteiropromo.core.address.controller;
+//
+//import ifpr.roteiropromo.core.address.model.entities.Address;
+//import ifpr.roteiropromo.core.address.service.AddressService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/address")
+//public class AddressController {
+//
+//
+//    private final AddressService addressService;
+//
+//    public AddressController(AddressService addressService){
+//        this.addressService = addressService;
+//    }
+//
+//    public ResponseEntity<Address> creatOne(
+//            @RequestBody Address address
+//    ){
+//        return ResponseEntity.ok(addressService.create(address));
+//    }
+//
+//
+//}
