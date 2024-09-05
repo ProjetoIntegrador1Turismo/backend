feb2250ac2455f0bce3a4e80f3e2f08a26c5384d
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-05 17:02:16 -0300

    chore: document schema AddressDTO

--- src/main/java/ifpr/roteiropromo/core/address/model/dtos/AddressDTO.java
+++ src/main/java/ifpr/roteiropromo/core/address/model/dtos/AddressDTO.java
@@ -1,12 +1,16 @@
 package ifpr.roteiropromo.core.address.model.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AddressDTO {
+    @Schema(description = "Information about street/avenue and city name", example = "Temple of the Sun Street - Iguaçu Falls")
     private String road;
+    @Schema(description = "Number location", example = "1234")
     private String number;
+    @Schema(description = "The postal zip code number", example = "85853-000")
     private String zipCode;
 }
