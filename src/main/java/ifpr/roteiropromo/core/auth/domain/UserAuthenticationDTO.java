a73a72a6369f090141b5ec881925cbf4bcf9f74f
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-10 11:45:00 -0300

    chore: document UserAuthenticationDTO

--- src/main/java/ifpr/roteiropromo/core/auth/domain/UserAuthenticationDTO.java
+++ src/main/java/ifpr/roteiropromo/core/auth/domain/UserAuthenticationDTO.java
@@ -1,11 +1,13 @@
 package ifpr.roteiropromo.core.auth.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserAuthenticationDTO {
+    @Schema(description = "User email", example = "admin@admin.com")
     private String username;
     private String password;
 }
