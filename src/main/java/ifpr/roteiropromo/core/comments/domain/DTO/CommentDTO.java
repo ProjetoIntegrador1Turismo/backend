2b4b7792b82772206728acbf81f3a10c734ae13c
Guilherme <guilherme_laz@hotmail.com>
2024-09-12 14:06:41 -0300

    feat: update to use BasicPointDTO instead of InterestPoint Entity

--- src/main/java/ifpr/roteiropromo/core/comments/domain/DTO/CommentDTO.java
+++ src/main/java/ifpr/roteiropromo/core/comments/domain/DTO/CommentDTO.java
@@ -2,6 +2,7 @@ package ifpr.roteiropromo.core.comments.domain.DTO;
 
 
 import ifpr.roteiropromo.core.interestPoint.domain.entities.InterestPoint;
+import ifpr.roteiropromo.core.pagesource.domain.BasicPointDTO;
 import ifpr.roteiropromo.core.user.domain.entities.Tourist;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
@@ -15,7 +16,8 @@ public class CommentDTO {
     private String text;
     private String wasVisitingDate;
     private Integer rating;
-    private InterestPoint interestPoint;
+//    private InterestPoint interestPoint;
+    private BasicPointDTO interestPoint;
     private String touristName;
 
 }
