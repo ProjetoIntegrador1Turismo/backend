389ff3df1a696c0ae27471de15233d785bf96653
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-13 10:04:18 -0300

    chore: document guide controller routes

--- src/main/java/ifpr/roteiropromo/core/user/controller/GuideController.java
+++ src/main/java/ifpr/roteiropromo/core/user/controller/GuideController.java
@@ -1,11 +1,19 @@
 package ifpr.roteiropromo.core.user.controller;
+import ifpr.roteiropromo.core.errors.StandartError;
 import ifpr.roteiropromo.core.itinerary.domain.dto.ItineraryDTO;
 import ifpr.roteiropromo.core.itinerary.domain.entities.Itinerary;
 import ifpr.roteiropromo.core.pagesource.domain.TopGuideDTO;
 import ifpr.roteiropromo.core.user.domain.dtos.GuideDTO;
+import ifpr.roteiropromo.core.user.domain.dtos.UserDTO;
 import ifpr.roteiropromo.core.user.domain.entities.Guide;
 import ifpr.roteiropromo.core.user.service.GuideService;
 import ifpr.roteiropromo.core.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +28,42 @@ import java.util.Map;
 @RequestMapping("/guides")
 @Log4j2
 @RequiredArgsConstructor
+@Tag(name = "Guide", description = "Operations dedicated to Guides")
 public class GuideController {
 
     private final GuideService guideService;
 
     @GetMapping()
     @PreAuthorize("hasRole('USER')")
+    @Operation(summary = "Get all Guides",
+            description = "Allow to get a list of all registered guides.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Guides found successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
     public ResponseEntity<List<GuideDTO>> getAllGuides(){
         return ResponseEntity.ok(guideService.getAllGuides());
     }
 
     @GetMapping("/itineraries")
     @PreAuthorize("hasRole('GUIA')")
+    @Operation(summary = "Get Guide Itineraries",
+            description = "Allow to get a list of all itineraries created by the authenticated guide.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Itineraries found successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class)) }),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = StandartError.class)) })
+    })
     public ResponseEntity<List<ItineraryDTO>> getItinerariesFromAuthenticatedGuide(){
         return ResponseEntity.ok(guideService.getItinerariesFromAuthenticatedGuide());
     }
