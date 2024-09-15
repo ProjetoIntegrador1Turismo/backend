8d5323880471caef8ecd6ef2969e4d5d0f2f69e0
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-15 00:36:15 -0300

    clean: remove unnecessary imports

--- src/test/java/ifpr/roteiropromo/interestPoint/controller/InterestPointControllerTest.java
+++ src/test/java/ifpr/roteiropromo/interestPoint/controller/InterestPointControllerTest.java
@@ -25,8 +25,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -130,13 +129,4 @@ public class InterestPointControllerTest {
                         status().isBadRequest()
                 );
     }
-
-
-
-
-
-
-
-
-
 }
