30d271594248de729d56c360e7474235206e6c21
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-14 23:14:41 -0300

    test: Unit test Interest Point controller. Method create -> should return InterestPoint created and status 200

---
+++ src/test/java/ifpr/roteiropromo/interestPoint/controller/InterestPointControllerTest.java
@@ -0,0 +1,92 @@
+package ifpr.roteiropromo.interestPoint.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ifpr.roteiropromo.core.interestPoint.controller.InterestPointController;
+import ifpr.roteiropromo.core.interestPoint.domain.dtos.InterestPointDTOForm;
+import ifpr.roteiropromo.core.interestPoint.domain.entities.InterestPoint;
+import ifpr.roteiropromo.core.interestPoint.domain.entities.TouristPoint;
+import ifpr.roteiropromo.core.interestPoint.service.InterestPointService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = InterestPointController.class, excludeAutoConfiguration = OAuth2ClientAutoConfiguration.class)
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class InterestPointControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    InterestPointService interestPointService;
+
+    ModelMapper mapper;
+    InterestPoint interestPoint;
+    InterestPointDTOForm payload;
+
+    @BeforeEach
+    public void setup(){
+
+        mapper = new ModelMapper();
+
+        payload = new InterestPointDTOForm();
+        payload.setName("Cataratas do Iguaçu");
+        payload.setInterestPointType("TOURIST_POINT");
+        payload.setShortDescription("Lorem ipsum lorem.");
+        payload.setDuration("2 horas");
+        payload.setAverageValue(4);
+        payload.setLongDescription("Lorem ipsum lorem, lorem lorem ipsum lorem.");
+
+        interestPoint = mapper.map(payload, TouristPoint.class);
+    }
+
+
+
+    @Test
+    public void createNewInterestPoint_shouldReturnInterestPointCreated() throws Exception {
+        given(interestPointService.create(ArgumentMatchers.any())).willReturn(interestPoint);
+
+        ResultActions actions = mockMvc.perform(post("/interestpoint")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(payload))
+                );
+
+        actions.andDo(print())
+            .andExpectAll(
+            status().isOk(),
+            jsonPath("$.name").value(payload.getName())
+        );
+    }
+
+
+
+
+
+
+
+
+
+}
