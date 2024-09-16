91c3ace41f4c7d9f29dc9e75a5efcdc33ffbe038
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-16 11:10:04 -0300

    clean: remove unused imports

--- src/test/java/ifpr/roteiropromo/auth/controller/AuthenticationControllerTest.java
+++ src/test/java/ifpr/roteiropromo/auth/controller/AuthenticationControllerTest.java
@@ -1,7 +1,6 @@
 package ifpr.roteiropromo.auth.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ifpr.roteiropromo.core.auth.controller.AuthenticationController;
 import ifpr.roteiropromo.core.auth.domain.AuthenticatedUserDTO;
